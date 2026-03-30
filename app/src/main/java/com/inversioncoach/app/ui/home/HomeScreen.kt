32bc9618acfa4c9f73ffc73cf411801176a2cf6e
Voycevestern <127464175+Voycepeh@users.noreply.github.com>
2026-03-30 10:56:15 +0800

    Fix Material3 experimental API compile error

--- app/src/main/java/com/inversioncoach/app/ui/home/HomeScreen.kt
+++ app/src/main/java/com/inversioncoach/app/ui/home/HomeScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
@@ -156,6 +157,7 @@ fun HomeScreen(
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 private fun Content(
     padding: PaddingValues,
     onStart: () -> Unit,
